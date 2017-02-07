147850bd761869c27ff4365a75ecd78b0c57de88
Damjan Pavlica <mudroljub@gmail.com>
2017-02-07 22:57:01 +0100

    m

---
+++ 25-trig-animacije/trig-animation.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('canvas')
+canvas.height = document.body.innerHeight || canvas.height
+canvas.width = document.body.clientWidth || canvas.width
+const ctx = canvas.getContext('2d')
+const modifier = 100
+
+const circle = {
+  x: 100,
+  y: 100,
+  r: 50
+}
+
+const drawCircle = circle => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.beginPath()
+  ctx.arc(circle.x, circle.y, circle.r, 0, 2 * Math.PI)
+  ctx.fill()
+}
+
+const loop = () => {
+  window.requestAnimationFrame(loop)
+  const time = Date.now() / 1000
+  circle.x = Math.cos(time) * modifier + canvas.width / 2
+  circle.y = Math.sin(time) * modifier + canvas.height / 2
+  drawCircle(circle)
+}
+
+loop()
