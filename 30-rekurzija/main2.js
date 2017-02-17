0eb365946111982795e562d90fa981796a4489a9
Damjan Pavlica <mudroljub@gmail.com>
2017-02-17 23:14:00 +0100

    rekurzija rekurzija...

--- 30-rekurzija/main2.js
+++ 30-rekurzija/main2.js
@@ -8,7 +8,8 @@ const drawCircle = (x, y, r) => {
   ctx.arc(x, y, r, 0, 2 * Math.PI)
   ctx.stroke()
   if (r < 2) return
-  drawCircle(x, y, r * 0.75)
+  drawCircle(x + r/2, y, r/2)
+  drawCircle(x - r/2, y, r/2)
 }
 
 drawCircle(canvas.width/2, canvas.height/2, 2000)
